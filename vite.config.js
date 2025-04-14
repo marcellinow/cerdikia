a06bb9fa07b2aef1bd7decd78aee3a48d2b75ed3
marcellinow <marcellino.gabriel12@gmail.com>
2025-04-14 23:57:35 +0700

    perbaikin base

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ const react = require("@vitejs/plugin-react");
 module.exports = defineConfig({
   plugins: [react()], // Add React plugin for Vite
   build: {
-    outDir: "../dist", // Output directory for the build
-    emptyOutDir: true,
     base: "/cerdikia/", // Clear the output directory before building
   },
 });
