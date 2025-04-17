4c7ab387e1bfb9f9284391c629e646ac39b30ea4
marcellinow <marcellino.gabriel12@gmail.com>
2025-04-17 21:23:38 +0700

    Configuration for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ const react = require("@vitejs/plugin-react");
 
 module.exports = defineConfig({
   plugins: [react()], // Add React plugin for Vite
-  build: {
-    base: "/cerdikia/", // Clear the output directory before building
-  },
+  base: "/cerdikia/", // Clear the output directory before building
 });
